8162b195fe4286bfedf9363e3aa38e5516129894
Irshad Ahmed <airshadahmedshaikh@gmail.com>
2024-03-31 22:45:37 +0500

    commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
 let myBalance = 15640;
 let myPin = 4455;
 console.log("Wellcome To IR ATM Machine");
-let pinAnswer = await inquirer.prompt([
+let pinAnswer = await inquirer_1.default.prompt([
     {
         name: "pin",
         message: "Enter Your Pin Code",
@@ -12,7 +17,7 @@ let pinAnswer = await inquirer.prompt([
 ]);
 if (pinAnswer.pin === myPin) {
     console.log("correct pin code");
-    let operationans = await inquirer.prompt([
+    let operationans = await inquirer_1.default.prompt([
         {
             name: "operation",
             message: "select your option",
@@ -21,7 +26,7 @@ if (pinAnswer.pin === myPin) {
         },
     ]);
     if (operationans.operation === "withdraw") {
-        let amountans = await inquirer.prompt([
+        let amountans = await inquirer_1.default.prompt([
             {
                 name: "amount",
                 message: "enter your amount",
@@ -40,7 +45,7 @@ if (pinAnswer.pin === myPin) {
         console.log(`your current balance is: ${myBalance}`);
     }
     else if (operationans.operation === "fastcash") {
-        let amountans = await inquirer.prompt([
+        let amountans = await inquirer_1.default.prompt([
             {
                 name: "amount",
                 message: "choose your amount",
